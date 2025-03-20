93dc47af8b58c5b165c6d5d026018ca608c788ea
lukasuxo <lukasukhiashvili06@gmail.com>
2025-03-20 13:44:58 +0400

    .

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "My App",
+  description: "A Next.js authentication project",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="bg-black text-white">{children}</body>
+    </html>
+  );
+}
